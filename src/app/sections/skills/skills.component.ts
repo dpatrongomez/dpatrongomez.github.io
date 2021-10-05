7c602d748395bd1b74d6751dadd3cadc15251707
Daniel Patrón Gómez <dpatrongomez@gmail.com>
2021-10-05 19:34:45 +0200

    Add skills

--- src/app/sections/skills/skills.component.ts
+++ src/app/sections/skills/skills.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Skill } from "./interfaces/skill.interface";
 
 @Component({
-  selector: 'section-skills',
-  templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  selector: "section-skills",
+  templateUrl: "./skills.component.html",
+  styleUrls: ["./skills.component.css"],
 })
 export class SkillsComponent implements OnInit {
+  skills: Skill[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.skills = [
+      { label: "Flutter", value: 80 },
+      { label: "Angular", value: 70 },
+      { label: "Android", value: 50 },
+      { label: "Java", value: 30 },
+      { label: "Python", value: 50 },
+      { label: "MySQL", value: 80 },
+    ];
   }
-
 }
